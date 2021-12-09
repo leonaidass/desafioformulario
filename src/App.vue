669116809c1f049a28ded1033bcdd3d63d1ad67b
leonaidass <leo22esquivel@gmail.com>
2021-12-08 23:00:13 -0300

    tabla y formulario

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,29 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+   <v-app-bar app color="green" dark>
+    <h1>Formulario</h1>
+   </v-app-bar>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+   <v-container>
+   <Formulario/>
+   <Tabla/>
+   </v-container>
 
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <HelloWorld/>
-    </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
-export default {
-  name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
-  data: () => ({
-    //
-  }),
-};
+import Formulario from "./components/Formulario.vue"
+import Tabla from "./components/Tabla.vue"
+  export default {
+    components:{
+      Formulario,
+      Tabla
+    }
+    
+  }
 </script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/Formulario.vue
@@ -0,0 +1,80 @@
+<template>
+  <div>
+    <h1>leoanrdo</h1>
+
+    <v-form  ref="productos" v-model="valid">
+      <v-container>
+        <v-row>
+          <v-col cols="12" md="4">
+            <v-text-field
+              v-model="firstname"
+              :rules="nameRules"
+              :counter="10"
+              label="Edad"
+              type="number"
+              required
+            ></v-text-field>
+          </v-col>
+
+          <v-col cols="12" md="4">
+            <v-text-field
+              v-model="firstname"
+              :rules="nameRules"
+              :counter="10"
+              label="First name"
+              type="date"
+              required
+            ></v-text-field>
+          </v-col>
+
+          <v-col cols="12" md="4">
+            <v-text-field
+              v-model="lastname"
+              :rules="nameRules"
+              :counter="10"
+              label="Last name"
+              required
+            ></v-text-field>
+          </v-col>
+
+          <v-col cols="12" md="4">
+            <v-text-field
+              v-model="email"
+              :rules="emailRules"
+              label="E-mail"
+              required
+            ></v-text-field>
+          </v-col>
+        </v-row>
+      </v-container>
+      <v-btn @click="enviarDatos()">Enviar</v-btn>
+    </v-form>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    valid: false,
+    firstname: "",
+    lastname: "",
+    nameRules: [
+      (v) => !!v || "Name is required",
+      (v) => v.length <= 10 || "Name must be less than 10 characters",
+    ],
+    email: "",
+    emailRules: [
+      (v) => !!v || "E-mail is required",
+      (v) => /.+@.+/.test(v) || "E-mail must be valid",
+    ],
+    datos:{}
+  }),
+  methods:{
+        enviarDatos(){
+           this.$refs.productos.validate()
+        }
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
